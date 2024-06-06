ca3e75964baa602ff54012c3154deff83f45aab0
sdevesh2617 <sdevesh069@gmail.com>
2024-06-06 13:19:19 +0530

    case studies header made dynamic

--- src/components/CaseStudiesHeader.tsx
+++ src/components/CaseStudiesHeader.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react"
-const CaseStudiesHeader: FC = ({ activeItem, setActiveItem }) => {
+
+interface CaseStudiesHeaderProps {
+  activeItem: string
+  setActiveItem: (item: string) => void
+}
+
+const CaseStudiesHeader: FC<CaseStudiesHeaderProps> = ({ activeItem, setActiveItem }) => {
   const caseStudiesHeader = [
     "Aerospace and Defense",
     "Automotive",
@@ -9,16 +15,23 @@ const CaseStudiesHeader: FC = ({ activeItem, setActiveItem }) => {
     "Rail Transportation",
     "Semiconductor",
   ]
-  // useEffect(() => {
-  // }, [])
+
   return (
-    <div className=" bg-sky-200 text-sky-800 font-semibold text-xl flex flex-row mt-24 fixed z-10 justify-around h-16 overflow-x-auto min-w-[100%] gap-4">
+    <div className="bg-sky-200 text-sky-800 font-semibold text-xl flex flex-row lg:mt-24 mt-20 fixed z-10 justify-around h-16 overflow-x-auto min-w-[100%] gap-8 whitespace-nowrap no-scrollbar">
       {caseStudiesHeader.map((item, index) => {
         return (
-          <p key={index} className={`${activeItem === item ? "text-blue-600 flex-shrink border-b-4 border-blue-600" : ""} h-full flex items-center cursor-pointer`} onClick={(e)=>{
-            document.getElementById(item).scrollIntoView({behavior:"smooth"})
-            setActiveItem(item)
-          }}>{item}</p>
+          <p 
+            key={index} 
+            className={`${
+              activeItem === item ? "text-blue-600 border-b-4 border-blue-600" : ""
+            } flex-shrink-0 h-full flex items-center cursor-pointer`} 
+            onClick={() => {
+              document.getElementById(item).scrollIntoView({ behavior: "smooth" })
+              setActiveItem(item)
+            }}
+          >
+            {item}
+          </p>
         )
       })}
     </div>
